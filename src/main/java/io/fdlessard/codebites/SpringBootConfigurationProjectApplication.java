e8ec421805a2b556fc78e2ef382c2a4c509dc861
fdlessard <fdlessard@gmail.com>
2016-11-09 20:35:58 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/SpringBootConfigurationProjectApplication.java
@@ -0,0 +1,12 @@
+package io.fdlessard.codebites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigurationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigurationProjectApplication.class, args);
+	}
+}
